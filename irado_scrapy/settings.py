21d655e7ad116b04052a8ac20f770a6debc49ff2
Maruno Ulfdrengr <maruno@ulfdrengr.nl>
2025-12-17 16:32:47 +0100

    Switch user-agent

--- irado_scrapy/settings.py
+++ irado_scrapy/settings.py
@@ -6,3 +6,4 @@ ITEM_PIPELINES = {
 EXTENSIONS = {
     'irado_scrapy.hass_discovery_ext.HassMQTTDiscoveryOnOpen': 100
 }
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Edge/79.0.1451.30 Safari/537.36'
